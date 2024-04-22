96ed00639ca2c7e24dd6d30ce37bf26e04fcee13
Simon Moinard <simmoinard@gmail.com>
2024-04-22 23:38:05 +0200

    Create UnderPressure_readval_raw.ino

---
+++ UnderPressure_readval_raw/UnderPressure_readval_raw.ino
@@ -0,0 +1,33 @@
+#include "Arduino.h"
+#include <algorithm>
+
+
+// pin connexions
+#define PIN_TRANSISTOR GPIO2
+#define PIN_PRESSURE ADC2
+
+
+// variables for sensor
+int val;
+float pressure, voltage;
+int calib = 557;
+
+///////////////////////////////////////////////////
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(PIN_TRANSISTOR, OUTPUT);
+  digitalWrite(PIN_TRANSISTOR, HIGH);
+}
+
+///////////////////////////////////////////////////
+void loop()
+{
+  //WakeUp and get battery Voltage
+
+  delay(50);
+  val = analogRead(PIN_PRESSURE); // Lecture de la valeur du capteur
+
+  Serial.print("Val :");
+  Serial.println(val);
+}
